aeb69b9128665fee89dd45f41043ff70290354d6
supercomet329 <supercomet329@gmail.com>
2022-05-25 12:34:48 +0900

    通常新規登録ページ　仮登録ボタン有効化/無効化切り替え処理修正

--- js/muse.js
+++ js/muse.js
@@ -1,8 +1,7 @@
 // 通常新規登録ページ(sign_up.html)
-var checkInput = function() {
+// 入力項目を確認し、仮登録ボタン有効化/無効化切り替え
+function checkInput() {
     // メールアドレスが入力された場合、emailCheckにtrueを格納
-    var email = document.getElementById('email');
-    email.addEventListener('change', checkInput);
 
     var emailCheck = '';
     if (document.getElementById('email').value.length > 0) {
@@ -13,7 +12,6 @@ var checkInput = function() {
 
     // 会員規約のチェックボックス要素を取得
     var terms = document.getElementById('terms');
-    terms.addEventListener('change', checkInput);
 
     // メールアドレスが入力されている、かつ会員規約にチェックがついている場合ボタンを有効化
     var registerBtn = document.getElementById('register-btn');
