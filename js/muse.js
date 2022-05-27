b17d1cf4a247b46a0fccf0e6cfccdacc1b274017
supercomet329 <supercomet329@gmail.com>
2022-05-27 11:30:01 +0900

    ログインページ作成

--- js/muse.js
+++ js/muse.js
@@ -31,3 +31,56 @@ function checkInput() {
         registerBtn.disabled = true;
     }
 }
+
+/**
+ * ログインページ(login.html)
+ */
+// 入力項目のフォーカスが外れた際に処理を実行
+$(function() {
+    // メールアドレスのフォーカスが外れた際にcheckLoginInput実行
+    $('#loginEmail').on('blur', function() {
+        checkLoginInput();
+    });
+    // パスワードのフォーカスが外れた際にcheckLoginInput実行
+    $('#loginPassword').on('blur', function() {
+        checkLoginInput();
+    })
+});
+
+// 入力項目を確認し、ログインボタン有効化/無効化切り替え
+function checkLoginInput() {
+    // ログインボタン有効化フラグ
+    var disabledFlag = true;
+
+    // 入力項目フラグ定義
+    var loginEmail = false;
+    var loginPassword = false;
+
+    // メールアドレスの値取得
+    var emailVal = $('#loginEmail').val();
+    // パスワードの値取得
+    var passwordVal = $('#loginPassword').val();
+
+    // メールアドレスが入力されているかを確認
+    if (emailVal.length > 0) {
+        // メールアドレスに空白文字が含まれていないかを確認
+        if (!emailVal.match(/[\x20\u3000]/)) {
+            // メールアドレスのフォーマットを確認
+            loginEmail = validateEmail(emailVal);
+        }
+    }
+    // パスワードが入力されているかを確認
+    if (passwordVal.length > 0) {
+        // パスワードに空白文字が含まれていないかを確認
+        if (!passwordVal.match(/[\x20\u3000/]/)) {
+            // パスワードのフォーマットを確認
+            loginPassword = validatePassword(passwordVal);
+        }
+    }
+
+    // 入力項目の値が正しい場合、ログインボタンを有効化
+    if (loginEmail === true && loginPassword === true) {
+        disabledFlag = false;
+    }
+    $('#login-btn').attr('disabled', disabledFlag);
+}
