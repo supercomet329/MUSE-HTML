8561b8d41e7d34c57480551cbe89bd45c101d295
supercomet329 <supercomet329@gmail.com>
2022-05-30 13:16:36 +0900

    メールアドレスのバリデート修正（「.」2つ以下に設定）&テキストレイアウトの修正

--- js/validate.js
+++ js/validate.js
@@ -15,5 +15,6 @@ function validatePassword(passwordVal) {
  */
 function validateEmail(emailVal) {
     // メールアドレス形式の場合trueを返す
-    return /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/.test(emailVal);
+    
+    return /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_-]{1,}\.[A-Za-z0-9]{1,}\.{0,1}[A-Za-z0-9]{1,}$/.test(emailVal);
 }
