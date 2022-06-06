5942d985765ce835286a7e9069385716b6c719e0
supercomet329 <supercomet329@gmail.com>
2022-06-06 18:36:51 +0900

    通常新規登録画面のJavascriptを修正 メールアドレスのフォームで全角スペースをチェックする処理を追加、メールアドレスのフォームでメールアドレスの形式をチェックする処理を追加

--- js/muse.js
+++ js/muse.js
@@ -18,22 +18,24 @@ $(function() {
 // 入力項目を確認し、仮登録ボタン有効化/無効化切り替え
 function checkInput() {
     // メールアドレスが入力された場合、emailCheckにtrueを格納
-    var emailCheck = '';
-    if (document.getElementById('email').value.length > 0) {
-        emailCheck = true;
-    } else {
-        emailCheck = false;
-    }
-
+    var emailCheck = false;
+    // メールアドレスの入力フォーム要素を取得
+    var email = document.getElementById('email');
     // 会員規約のチェックボックス要素を取得
     var terms = document.getElementById('terms');
+    // 仮登録のボタン要素を取得
+    var registerBtn = document.getElementById('register-btn');
+
+    // メールアドレスに空白文字が含まれていないかを確認
+    if (!email.value.match(/[\x20\u3000]/)) {
+        // メールアドレスのフォーマットを確認
+        emailCheck = validateEmail(email.value);
+    }
 
     // メールアドレスが入力されている、かつ会員規約にチェックがついている場合ボタンを有効化
-    var registerBtn = document.getElementById('register-btn');
+    registerBtn.disabled = true;
     if (emailCheck === true && terms.checked === true) {
         registerBtn.disabled = false;
-    } else {
-        registerBtn.disabled = true;
     }
 }
 
