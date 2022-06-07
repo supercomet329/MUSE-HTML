603487a6e3cfcbbee751f67016ea8c671adf306e
supercomet329 <supercomet329@gmail.com>
2022-06-07 13:08:57 +0900

    タイルボタン押下時にトップページ(timeline.html)に遷移、フォローボタン処理、フォローボタンレイアウト修正、画像に枠線追加、プロフィール詳細追加に伴うレイアウト修正

--- js/muse.js
+++ js/muse.js
@@ -949,3 +949,18 @@ $(function() {
         $('.modal').fadeOut();
     });
 });
+
+
+// フォローする、フォロー中の選択機能
+$(function () {
+    $(document).on('click', '.follow-off', function() {
+      let follow_on = $('<button type="button"class="btn btn-primary rounded-pill btn-sm text-white btn-lg main-color follow-btn follow-on">フォロー中</button>');
+      $(this).replaceWith(follow_on);
+    });
+
+    $(document).on('click', '.follow-on', function() {
+      let follow_off = $('<button type="button" class="btn rounded-pill btn-outline-primary btn-sm follow-btn follow-off">フォローする</button>');
+      $(this).replaceWith(follow_off);
+    });
+});
+
