2fbdaf9ddec2467d6bac75fc7d06aabbdc3dad79
supercomet329 <supercomet329@gmail.com>
2022-06-14 20:49:08 +0900

    # パスワードのチェックの変更 ## 大文字・小文字・数字 が最低1文字ずつ入っている かつ 8から32文字

--- js/validate.js
+++ js/validate.js
@@ -5,7 +5,8 @@
  */
 function validatePassword(passwordVal) {
     // 半角英小文字、大文字、数字を含む9文字以上32文字以内であればtrueを返す
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{9,32}$/.test(passwordVal);
+    // return /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{9,32}$/.test(passwordVal);
+    return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,32}$/.test(passwordVal);
 }
 
 /**
