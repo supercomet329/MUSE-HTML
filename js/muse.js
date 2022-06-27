2d6a64fa7d6e0af10f69d1defb01a65eeaf2887c
supercomet329 <supercomet329@gmail.com>
2022-06-27 18:04:36 +0900

    ランキングページ作成

--- js/muse.js
+++ js/muse.js
@@ -1584,3 +1584,16 @@ $(function() {
     });
     
 });
+
+// tableの列にリンク設定（ranking.html）
+$( function($) {
+    $('tbody tr[data-href]').addClass('clickable').click( function() {
+        window.location = $(this).attr('data-href');
+    }).find('a').hover( function() {
+        $(this).parents('tr').unbind('click');
+    }, function() {
+        $(this).parents('tr').click( function() {
+            window.location = $(this).attr('data-href');
+        });
+    });
+});
